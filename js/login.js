82b504a186bccdc78ca6fb21d136cb72386706a0
leonnelleijing <59120896+leonnelleijing@users.noreply.github.com>
2020-04-19 10:37:34 +0200

    fic bug

---
+++ js/login.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+		$('#log').click(function(){
+		var email=$('#email').val();
+		var mdp=$('#mdp').val();
+		$.ajax({
+			type:'post',
+	            url:'verifier.php',
+	            data:{
+	                email:email ,
+	                mdp:mdp
+	            },
+	        success: function(res){
+	        	// console.log(res);
+	        	var info=JSON.parse(res).info;
+	        	var dir=JSON.parse(res).directeur;
+	        	var id=JSON.parse(res).id;
+	        	switch(info){
+	        		case 0:
+	        			alert('Vérifier votre connexion internet');
+	        		break;
+
+	        		case 1:
+	        			$('#email').addClass('is-invalid');
+                                        $('#feedback-mail').html("Ce mail n'existe pas");
+	        		break;
+
+	        		case 2:
+	        			$('#mdp').addClass('is-invalid');
+        				$('#feedback-mdp').html("Mot de passe incorrect");
+	        		break;
+
+	        		case 3:
+	        			if(dir==0){
+	        			  // console.log(dir);
+	        			  window.location.href = './utilisateur/utilisateur.php?id='+id;
+	        			}else{
+	        			  // console.log(dir);
+	        			  window.location.href = './directeur/admin.php?id='+id;
+	        			}	
+	        		break;
+	        	}
+
+	        }
+		})
+	})
+
+})
